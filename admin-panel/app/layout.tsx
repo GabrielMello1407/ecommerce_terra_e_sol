799d705a158d4cdce0183c2c985ad3123a15b0d7
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2024-09-03 14:33:46 -0300

    feat: added clerk auth in admin panel

--- admin-panel/app/layout.tsx
+++ admin-panel/app/layout.tsx
@@ -1,6 +1,15 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import {
+  ClerkProvider,
+  SignedOut,
+  SignInButton,
+  UserButton,
+  SignedIn,
+  SignOutButton,
+} from '@clerk/nextjs';
+import { ptBR } from '@clerk/localizations';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,8 +24,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider localization={ptBR}>
+      <html lang="en">
+        <body>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
