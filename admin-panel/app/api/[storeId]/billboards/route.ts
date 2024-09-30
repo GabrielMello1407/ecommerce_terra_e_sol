8a7230319138164d0638fa6bc0b8bb03158b3492
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2024-09-30 16:09:20 -0300

    delete: removed delete from this route

--- admin-panel/app/api/[storeId]/billboards/route.ts
+++ admin-panel/app/api/[storeId]/billboards/route.ts
@@ -72,32 +72,3 @@ export async function GET(
     return new NextResponse('Internal Error', { status: 500 });
   }
 }
-
-export async function DELETE(
-  req: Request,
-  { params }: { params: { storeId: string } },
-) {
-  try {
-    const { userId } = auth();
-
-    if (!userId) {
-      return new NextResponse('Unauthenticated', { status: 401 });
-    }
-
-    if (!params.storeId) {
-      return new NextResponse('Id da loja é obrigatório', { status: 400 });
-    }
-
-    const store = await prismadb.store.deleteMany({
-      where: {
-        id: params.storeId,
-        userId,
-      },
-    });
-
-    return NextResponse.json(store);
-  } catch (error) {
-    console.log('[STORE_DELETE]', error);
-    return new NextResponse('Internal Error', { status: 500 });
-  }
-}
