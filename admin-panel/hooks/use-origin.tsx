13aacba0db87ecf7565b2a9bdb691e24b07edb06
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2024-10-10 14:50:55 -0300

    fix: fixed bug route

--- admin-panel/hooks/use-origin.tsx
+++ admin-panel/hooks/use-origin.tsx
@@ -1,15 +1,12 @@
 import { useEffect, useState } from 'react';
 
 export const useOrigin = () => {
-  const [mounted, setMounted] = useState(false);
-
-  const origin =
-    typeof window !== 'undefined' && window.location.origin
-      ? window.location.origin
-      : '';
+  const [origin, setOrigin] = useState('');
 
   useEffect(() => {
-    setMounted(true);
+    if (typeof window !== 'undefined') {
+      setOrigin(window.location.origin);
+    }
   }, []);
 
   return origin;
