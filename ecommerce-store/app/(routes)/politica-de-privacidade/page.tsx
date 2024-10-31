1e14d5942ea4fb12df406471e32ae1f0cd9e21c7
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2024-10-31 17:03:32 -0300

    feat: create PrivacyPolicy

---
+++ ecommerce-store/app/(routes)/politica-de-privacidade/page.tsx
@@ -0,0 +1,222 @@
+const PrivacyPolicy = () => {
+  return (
+    <div className="bg-white py-10 px-6 lg:px-32">
+      <h1 className="text-3xl font-bold text-green-900 mb-6 text-center">
+        Política de Privacidade da Terra & Sol Confecções
+      </h1>
+      <hr className="mb-6" />
+      <div className="flex flex-col lg:flex-row items-start gap-8">
+        <div className="w-full lg:w-2/3 text-green-900">
+          <div className="mb-4">
+            <h1>
+              Última atualização: <span className="font-bold ">31/10/2024</span>
+            </h1>
+            <div className="mb-4 mt-4">
+              <p>
+                A <span className="font-bold">Terra & Sol Confecções </span>
+                valoriza a privacidade de seus clientes e se compromete a
+                proteger suas informações pessoais. Esta Política de Privacidade
+                descreve como coletamos, usamos, armazenamos e protegemos suas
+                informações quando você interage com nossos serviços, incluindo
+                nosso site e nossos produtos.
+              </p>
+            </div>
+            <h2 className="font-semibold">1. Quem somos?</h2>
+            <p>
+              Terra & Sol Confecções é uma empresa especializada na produção de
+              uniformes personalizados e padronizados, atendendo às necessidades
+              de empresas de diversos setores e marcas de roupas. Nossa missão é
+              fornecer uniformes que reflitam a identidade de cada cliente,
+              combinando conforto, durabilidade e design inovador.
+            </p>
+          </div>
+
+          <div className="mb-4">
+            <h2 className="font-semibold">2. Informações que Coletamos</h2>
+            <p>Coletamos diferentes tipos de informações, incluindo:</p>
+            <span className="font-bold">Informações Pessoais:</span>
+            <ul className="list-disc mb-2">
+              <li>Nome completo</li>
+              <li>Endereço (físico e de entrega)</li>
+              <li>Número de telefone</li>
+              <li>Endereço de e-mail</li>
+              <li>Informações da empresa (CNPJ, nome da empresa, etc.)</li>
+            </ul>
+            <span className="font-bold ">Informações de Navegação:</span>
+            <ul className="list-disc mb-2">
+              <li>Endereço IP</li>
+              <li>Tipo de navegador</li>
+              <li>Páginas visitadas em nosso site</li>
+              <li>Tempo de permanência em cada página</li>
+              <li>Referências de sites (sites que o encaminharam para nós)</li>
+            </ul>
+          </div>
+
+          <div className="mb-4">
+            <h2 className="font-semibold">3. Como Usamos Suas Informações</h2>
+            <p className="font-bold">Utilizamos suas informações para:</p>
+            <ul className="list-disc mb-2">
+              <li>Processar e gerenciar pedidos de produtos e serviços.</li>
+              <li>
+                Comunicar sobre o status de seus pedidos, confirmações e
+                atualizações.
+              </li>
+              <li>
+                Personalizar sua experiência em nosso site e oferecer produtos e
+                serviços que sejam relevantes para você.
+              </li>
+              <li>
+                Melhorar nossos serviços e a usabilidade do nosso site através
+                de análises de dados.
+              </li>
+              <li>
+                Enviar comunicações de marketing e promoções, se você optar por
+                recebê-las.
+              </li>
+              <li>Responder a suas perguntas e oferecer suporte ao cliente.</li>
+            </ul>
+          </div>
+
+          <div className="mb-4">
+            <h2 className="font-semibold">
+              4. Compartilhamento de Informações
+            </h2>
+            <p>
+              A Terra & Sol Confecções não vende, troca ou aluga suas
+              informações pessoais a terceiros. Podemos compartilhar suas
+              informações com:
+            </p>
+            <ul className="list-disc mb-2">
+              <li>
+                <span className="font-bold">Prestadores de Serviços:</span>{' '}
+                Empresas que nos ajudam a operar nosso site, processar
+                pagamentos, entregar produtos e enviar e-mails. Essas empresas
+                têm acesso limitado às suas informações e estão obrigadas a
+                protegê-las.
+              </li>
+              <li>
+                <span className="font-bold">Cumprimento Legal:</span> Podemos
+                divulgar suas informações se exigido por lei ou em resposta a
+                solicitações legais, como ordens judiciais ou intimações.
+              </li>
+            </ul>
+          </div>
+
+          <div className="mb-4">
+            <h2 className="font-semibold">5. Segurança das Informações</h2>
+            <p>
+              Implementamos medidas de segurança apropriadas para proteger suas
+              informações pessoais contra acesso não autorizado, uso indevido ou
+              divulgação. Isso inclui o uso de tecnologias de criptografia,
+              firewalls e práticas de segurança física.
+            </p>
+          </div>
+
+          <div className="mb-4">
+            <h2 className="font-semibold">6. Retenção de Dados</h2>
+            <p>
+              Retemos suas informações pessoais apenas pelo tempo necessário
+              para cumprir os propósitos para os quais foram coletadas,
+              incluindo a finalidade de satisfazer requisitos legais, contábeis
+              ou de relatório.
+            </p>
+          </div>
+
+          <div className="mb-4">
+            <h2 className="font-semibold">7. Seus Direitos</h2>
+            <p className="font-bold">
+              Você tem os seguintes direitos em relação às suas informações
+              pessoais:
+            </p>
+            <ul className="list-disc mb-2">
+              <li>
+                Acesso: Solicitar uma cópia das informações pessoais que temos
+                sobre você.
+              </li>
+              <li>
+                Correção: Solicitar a correção de informações pessoais
+                imprecisas ou incompletas.
+              </li>
+              <li>
+                Exclusão: Solicitar a exclusão de suas informações pessoais,
+                quando não houver mais necessidade de mantê-las.
+              </li>
+              <li>
+                Restrição: Solicitar a restrição do processamento de suas
+                informações em determinadas circunstâncias.
+              </li>
+              <li>
+                Portabilidade: Solicitar a transferência de suas informações
+                pessoais para outro controlador.
+              </li>
+            </ul>
+          </div>
+
+          <div className="mb-4">
+            <h2 className="font-semibold">
+              8. Cookies e Tecnologias Semelhantes
+            </h2>
+            <p>
+              Utilizamos cookies e tecnologias semelhantes para melhorar a
+              experiência do usuário em nosso site, analisar o tráfego e
+              entender como os visitantes interagem com nosso conteúdo. Você
+              pode ajustar as configurações do seu navegador para recusar
+              cookies, mas isso pode afetar a funcionalidade do site.
+            </p>
+          </div>
+
+          <div className="mb-4">
+            <h2 className="font-semibold">9. Links para Outros Sites</h2>
+            <p>
+              Nosso site pode conter links para outros sites que não são
+              operados por nós. Não somos responsáveis pela privacidade e pelas
+              práticas de segurança de outros sites. Recomendamos que você
+              revise a política de privacidade de cada site que visita.
+            </p>
+          </div>
+
+          <div className="mb-4">
+            <h2 className="font-semibold">
+              10. Alterações a Esta Política de Privacidade
+            </h2>
+            <p>
+              Podemos atualizar esta Política de Privacidade periodicamente.
+              Quaisquer alterações serão publicadas nesta página, e recomendamos
+              que você a revise regularmente. A data da última atualização
+              estará sempre visível no início desta política.
+            </p>
+          </div>
+
+          <div className="mb-4">
+            <h2 className="font-semibold">11. Contato</h2>
+            <p>
+              Se você tiver dúvidas ou preocupações sobre esta Política de
+              Privacidade ou sobre como tratamos suas informações pessoais,
+              entre em contato conosco:
+            </p>
+            <p>
+              <span className="font-bold">Terra & Sol Confecções</span>
+              <br />
+              E-mail:{' '}
+              <a
+                href="mailto:terraesolmoda@yahoo.com.br"
+                className="text-green-700"
+              >
+                terraesolmoda@yahoo.com.br
+              </a>
+              <br />
+              Telefone: <span className="text-green-700">(43) 9 9127-7048</span>
+              <br />
+              Endereço:{' '}
+              <span className="text-green-700">
+                Rua Paraná-454, Centro, Jacarezinho-PR
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
