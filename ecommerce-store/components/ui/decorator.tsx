af0b69a1860e91848df38c7fd7a0f8c26f2fdbf8
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2024-10-31 17:04:29 -0300

    path: added props in decorator

--- ecommerce-store/components/ui/decorator.tsx
+++ ecommerce-store/components/ui/decorator.tsx
@@ -1,9 +1,19 @@
 import Image from 'next/image';
 
-const Decorator = () => {
+interface DecoratorProps {
+  width: number;
+  height: number;
+}
+
+const Decorator: React.FC<DecoratorProps> = ({ width, height }) => {
   return (
     <div>
-      <Image alt="Decorator" src="/decorator.svg" width={4} height={25} />
+      <Image
+        alt="Decorator"
+        src="/decorator.svg"
+        width={width}
+        height={height}
+      />
     </div>
   );
 };
