f411aa09b380218ef2fc4bca524756cbc5344db4
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2024-11-05 16:14:09 -0300

    path: changed text in info page

--- ecommerce-store/components/info.tsx
+++ ecommerce-store/components/info.tsx
@@ -16,10 +16,10 @@ interface CartItem extends Product {
   selectedColor: Color | null;
   selectedSize: Size | null;
 }
+
 const Info: React.FC<InfoProps> = ({ data }) => {
   const cart = useCart();
 
-  // Estado para armazenar a cor e o tamanho selecionados
   const [selectedColor, setSelectedColor] = useState<Color | null>(null);
   const [selectedSize, setSelectedSize] = useState<Size | null>(null);
 
@@ -38,6 +38,7 @@ const Info: React.FC<InfoProps> = ({ data }) => {
 
   return (
     <div className="space-y-4">
+      {/* Informações principais do produto */}
       <div className="flex items-center gap-x-2">
         <Decorator width={5} height={25} />
         <h1 className="text-3xl font-bold text-[#025213] font-roboto">
@@ -46,31 +47,27 @@ const Info: React.FC<InfoProps> = ({ data }) => {
       </div>
 
       <div className="flex items-center gap-2">
-        <div className="flex text-green-700">
-          {/* Session assessment */}
-          ★★★★☆
-        </div>
+        <div className="flex text-green-700">★★★★☆</div>
         <p className="text-sm text-gray-500 font-roboto">(150)</p>
       </div>
 
-      {/* Session Price */}
       <div className="mt-3 flex items-end justify-between">
         <p className="text-3xl font-bold text-[#025213] font-roboto">
           <Currency value={data.price} />
         </p>
       </div>
 
-      {/* Session description */}
       <div className="flex items-center gap-x-4">
         <div className="text-gray-600 font-roboto">
           {data.description?.value}
         </div>
       </div>
+
       <h3 className="font-bold text-black font-roboto">
         {data.description?.name}
       </h3>
 
-      {/* Session sizes */}
+      {/* Seção de tamanhos */}
       <div className="mt-4">
         <h3 className="font-semibold text-black font-roboto">Tamanhos</h3>
         <div className="flex gap-2 mt-2">
@@ -80,7 +77,7 @@ const Info: React.FC<InfoProps> = ({ data }) => {
               className={`border p-2 rounded text-center w-10 h-10 cursor-pointer ${
                 selectedSize?.id === size.id ? 'bg-green-200' : ''
               }`}
-              onClick={() => setSelectedSize(size)} // Atualiza o tamanho selecionado
+              onClick={() => setSelectedSize(size)}
             >
               {size.value}
             </div>
@@ -88,7 +85,7 @@ const Info: React.FC<InfoProps> = ({ data }) => {
         </div>
       </div>
 
-      {/* Session colors */}
+      {/* Seção de cores */}
       <div className="mt-4">
         <h3 className="font-semibold text-black font-roboto">Cores</h3>
         <div className="flex gap-2 mt-2">
@@ -99,12 +96,13 @@ const Info: React.FC<InfoProps> = ({ data }) => {
                 selectedColor?.id === colors.id ? 'ring-2 ring-blue-500' : ''
               }`}
               style={{ backgroundColor: colors.value }}
-              onClick={() => setSelectedColor(colors)} // Atualiza a cor selecionada
+              onClick={() => setSelectedColor(colors)}
             />
           )) || 'Nenhuma cor disponível'}
         </div>
       </div>
 
+      {/* Botão de adicionar ao carrinho */}
       <div className="mt-10 flex items-center gap-x-3">
         <Button
           className="flex items-center gap-x-2 rounded-md bg-[#025213] font-roboto"
