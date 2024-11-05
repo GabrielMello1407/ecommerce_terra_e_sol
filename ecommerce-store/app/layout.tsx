58159f163b4fff61e646904d3f95b645545dbd89
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2024-11-05 16:13:17 -0300

    feat: added auth provider

--- ecommerce-store/app/layout.tsx
+++ ecommerce-store/app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from '@/components/footer';
 import Navbar from '@/components/navbar';
 import ModalProvider from '@/providers/modal-provider';
 import ToastProvider from '@/providers/toast-provider';
+import { AuthProvider } from '@/providers/auth-provider';
 
 const font = Urbanist({ subsets: ['latin'] });
 
@@ -21,11 +22,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <ModalProvider />
-        <ToastProvider />
-        <Navbar />
-        {children}
-        <Footer />
+        <AuthProvider>
+          <ModalProvider />
+          <ToastProvider />
+          <Navbar />
+          {children}
+          <Footer />
+        </AuthProvider>
       </body>
     </html>
   );
