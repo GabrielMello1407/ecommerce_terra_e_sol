d92350872b1e0a7ec28d6b7a9f62a6833ff4604c
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2024-11-19 14:16:46 -0300

    update: changed route to billboard

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -9,12 +9,12 @@ export const revalidate = 0;
 const Homepage = async () => {
   const products = await getProducts({ isFeatured: true });
 
-  const billboard = await getBillboard('20adc874-9896-46ba-a901-bf497ae3e270');
+  const billboard = await getBillboard('b7e66668-13cc-4994-ad43-7e95354f1935');
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard data={billboard} />
+        <Billboard data={billboard} title="Compre Agora" link="/produtos" />
 
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Últimos produtos" items={products} />
