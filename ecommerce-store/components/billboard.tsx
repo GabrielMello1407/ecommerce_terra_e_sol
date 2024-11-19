b398d5c8b94fb384dc44510e6c30c9fa99de768d
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2024-11-19 14:18:09 -0300

    update: added new fuctions for this component

--- ecommerce-store/components/billboard.tsx
+++ ecommerce-store/components/billboard.tsx
@@ -1,24 +1,36 @@
+/* eslint-disable @next/next/no-img-element */
 import { Billboard as BillboardType } from '@/types';
+import Link from 'next/link';
 
 interface BillboardProps {
   data: BillboardType;
+  title: string;
+  link: string;
 }
 
-const Billboard: React.FC<BillboardProps> = ({ data }) => {
+const Billboard: React.FC<BillboardProps> = ({ data, title, link }) => {
   return (
     <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
       <div
-        className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover"
-        style={{
-          backgroundImage: `url(${data?.imageUrl || '/path/to/default.jpg'})`,
-        }}
+        className="rounded-xl relative overflow-hidden"
+        style={{ height: '400px' }}
       >
-        <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
+        <img
+          src={data?.imageUrl || '/path/to/default.jpg'}
+          alt={data?.label || 'Título padrão'}
+          className="w-full h-full object-cover"
+        />
+        <div className="absolute inset-0 flex flex-col justify-center items-center text-center gap-y-8">
           <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs text-white">
             {data?.label || 'Título padrão'}
           </div>
           <h3 className="font-bold text-xl sm:text-4xl lg:text-4xl sm:max-w-xl max-w-xs line-through text-white">
-            Compre agora!
+            <Link
+              href={link}
+              className="hover:text-gray-200 transition-colors duration-300"
+            >
+              {title}
+            </Link>
           </h3>
         </div>
       </div>
